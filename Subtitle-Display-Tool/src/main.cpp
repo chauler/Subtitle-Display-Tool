0e3471e886aa5f5b296603a060490d6ad2070648
Alex Tomjack <amt1309@gmail.com>
2024-06-20 21:17:00 -0500

    Added raylib and wrote basic initial code for creating a window.

--- Subtitle-Display-Tool/src/main.cpp
+++ Subtitle-Display-Tool/src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "raylib.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	const int screenWidth = 575;
+	const int screenHeight = 100;
+
+	SetConfigFlags(FLAG_WINDOW_TOPMOST | FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_UNFOCUSED | FLAG_WINDOW_MOUSE_PASSTHROUGH | FLAG_VSYNC_HINT);
+	InitWindow(screenWidth, screenHeight, "Subtitle Display Tool");
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		DrawText("Hello World", 0, 0, 100, LIGHTGRAY);
+		EndDrawing();
+	}
+	return 0;
 }
