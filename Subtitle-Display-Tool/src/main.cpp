bc15233a8cfa594ab7e9f40e26a348504f1f7900
Alex Tomjack <amt1309@gmail.com>
2024-06-27 19:30:37 -0500

    Made window transparent

--- Subtitle-Display-Tool/src/main.cpp
+++ Subtitle-Display-Tool/src/main.cpp
@@ -5,11 +5,13 @@ int main() {
 	const int screenWidth = 575;
 	const int screenHeight = 100;
 
-	SetConfigFlags(FLAG_WINDOW_TOPMOST | FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_UNFOCUSED | FLAG_WINDOW_MOUSE_PASSTHROUGH | FLAG_VSYNC_HINT);
+	SetConfigFlags(FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_TOPMOST | FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_UNFOCUSED | FLAG_WINDOW_MOUSE_PASSTHROUGH | FLAG_VSYNC_HINT);
 	InitWindow(screenWidth, screenHeight, "Subtitle Display Tool");
-
+	unsigned int i = 0;
+	RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
 	while (!WindowShouldClose()) {
 		BeginDrawing();
+		ClearBackground(BLANK);
 		DrawText("Hello World", 0, 0, 100, LIGHTGRAY);
 		EndDrawing();
 	}
