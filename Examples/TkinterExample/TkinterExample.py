ff8a40b881237a2025e880a9af53a97d71b42654
Alex Tomjack <amt1309@gmail.com>
2024-07-18 20:39:35 -0500

    Fleshed out python example host

--- Examples/TkinterExample/TkinterExample.py
+++ Examples/TkinterExample/TkinterExample.py
@@ -14,13 +14,7 @@ app = Tk()
 app.title("Example Tkinter Host App")
 app.geometry('350x800')
 
-def clicked():
-    data = {
-        "mode": "simple",
-        "data": {
-            "dialogue": "This string was sent from the Tkinter Host App"
-        }
-    }
+def clicked(data):
     #Send JSON to our program
     try:
         # Connect to server and send data
@@ -40,8 +34,42 @@ def clicked():
     if received:
         print("Received: {}".format(received))
  
-btn = Button(app, text = "Create window using simple mode", fg = "black", command=clicked)
+btn = Button(app, text = "Create window using simple mode", fg = "black", command=lambda: clicked({
+        "mode": "simple",
+        "data": {
+            "dialogue": "This string was sent from the Tkinter Host App"
+        }
+    }))
+
+btn2 = Button(app, text = "Create a stylized window with our API", fg = "black", command=lambda: clicked({
+        "mode": "advanced",
+        "data": {
+            "dialogue": "This string was sent from the Tkinter Host App",
+            "styles": {
+                "fontSize": 36,
+                "position": [250, 250],
+			    "fontColor": [51, 255, 255, 255],
+			    "bgColor": [102, 0, 51, 255]
+            }
+        }
+    }))
+
+btn3 = Button(app, text = "Create a transparent window with a custom font", fg = "black", command=lambda: clicked({
+        "mode": "advanced",
+        "data": {
+            "dialogue": "This string was sent from the Tkinter Host App",
+            "styles": {
+                "fontSize": 36,
+                "position": [250, 250],
+			    "fontColor": [255, 255, 255, 255],
+			    "bgColor": [102, 0, 51, 0],
+			    "fontPath": "C:\\Windows\\Fonts\\Calibri.ttf",
+            }
+        }
+    }))
 
 btn.grid(column=0, row=0)
+btn2.grid(column=0, row=1)
+btn3.grid(column=0, row=2)
 
 app.mainloop()
