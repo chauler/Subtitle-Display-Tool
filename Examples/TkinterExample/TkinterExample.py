1ef9377d31364c9d9a9cb2135193366a30eb2de7
Alex Tomjack <amt1309@gmail.com>
2024-07-18 21:13:57 -0500

    Further additions to sample Tkinter host

--- Examples/TkinterExample/TkinterExample.py
+++ Examples/TkinterExample/TkinterExample.py
@@ -9,7 +9,6 @@ args = ['..\\..\\bin\\Debug\\Subtitle-Display-Tool.exe', str(PORT)]
 subprocess.Popen(args)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 app = Tk()
 app.title("Example Tkinter Host App")
 app.geometry('350x800')
@@ -21,18 +20,10 @@ def clicked(data):
         sock.connect((HOST, PORT))
         sock.sendall(bytes(json.dumps(data),encoding="utf-8"))
 
-
-        # Receive data from the server and shut down
-        sock.settimeout(1)
-        received = sock.recv(1024)
-        received = received.decode("utf-8")
-
     finally:
         sock.close()
         
     print("Sent:     {}".format(data))
-    if received:
-        print("Received: {}".format(received))
  
 btn = Button(app, text = "Create window using simple mode", fg = "black", command=lambda: clicked({
         "mode": "simple",
@@ -49,7 +40,8 @@ btn2 = Button(app, text = "Create a stylized window with our API", fg = "black",
                 "fontSize": 36,
                 "position": [250, 250],
 			    "fontColor": [51, 255, 255, 255],
-			    "bgColor": [102, 0, 51, 255]
+			    "bgColor": [102, 0, 51, 255],
+                "lifetime": 0
             }
         }
     }))
@@ -68,8 +60,24 @@ btn3 = Button(app, text = "Create a transparent window with a custom font", fg =
         }
     }))
 
+btn4 = Button(app, text = "Create a window which lasts 0.5 seconds", fg = "black", command=lambda: clicked({
+        "mode": "advanced",
+        "data": {
+            "dialogue": "This string was sent from the Tkinter Host App",
+            "styles": {
+                "fontSize": 36,
+                "position": [250, 250],
+			    "fontColor": [255, 255, 255, 255],
+			    "bgColor": [102, 0, 51, 0],
+			    "fontPath": "C:\\Windows\\Fonts\\Calibri.ttf",
+                "lifetime": 0.5
+            }
+        }
+    }))
+
 btn.grid(column=0, row=0)
 btn2.grid(column=0, row=1)
 btn3.grid(column=0, row=2)
+btn4.grid(column=0, row=3)
 
 app.mainloop()
