44fc38fa5da24f7587d63ad085184f9745a2172f
Will Haley <whaley2409@gmail.com>
2024-08-16 14:07:30 -0500

    Created more test for private members of InputManager

--- Subtitle-Display-Tool-Tests/InputManagerTest.cpp
+++ Subtitle-Display-Tool-Tests/InputManagerTest.cpp
@@ -4,6 +4,15 @@
 #include<string>
 #include<thread>
 
+int GetPortNum(InputManager& im) {
+
+	return im.m_portNum;
+}
+
+bool GetAcceptorOpen(InputManager& im) {
+	return im.acceptor_.is_open();
+}
+
 
 class InputManagerTest : public testing::Test {
 protected:
@@ -12,6 +21,19 @@ protected:
 	}
 };
 
+TEST_F(InputManagerTest, CheckAcceptor) {
+	//set-up io_context and thread
+	asio::io_context io_context;
+	InputManager im{ 9999,io_context };
+	asio::io_context::work idleWork(io_context);
+	std::thread ioThread = std::thread([&]() { io_context.run(); });
+	// test whether the acceptor has the socket open
+	bool isCreated = GetAcceptorOpen(im);
+	// tear down io_context and its thread
+	io_context.stop();
+	ioThread.join();
+	EXPECT_EQ(isCreated, true);
+}
 
 TEST_F(InputManagerTest, CheckManagerCreation) {
 	std::string empty_string = "";
@@ -27,4 +49,18 @@ TEST_F(InputManagerTest, CheckManagerCreation) {
 	ioThread.join();
 	// string from getData should be empty because no host has connected
 	EXPECT_EQ(empty_string, empty_socket_message);
+}
+
+TEST_F(InputManagerTest, CheckPortNum) {
+	// set-up io_context and thread
+	asio::io_context io_context;
+	InputManager im{ 9999,io_context };
+	asio::io_context::work idleWork(io_context);
+	std::thread ioThread = std::thread([&]() { io_context.run(); });
+	// should return the private variable that stores the port number
+	int current_portNum = GetPortNum(im);
+	// tear down io_context and its thread
+	io_context.stop();
+	ioThread.join();
+	EXPECT_EQ(current_portNum, 9999);
 }
